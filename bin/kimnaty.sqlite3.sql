83015c0c92ab78ab7bfb7613decad0510018d363
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-03-20 11:15:50 +0100

    new file

---
+++ bin/kimnaty.sqlite3.sql
@@ -0,0 +1,40 @@
+# SQLite3 script
+# create table for Mija LYWSD03MMC device readings
+
+DROP TABLE IF EXISTS data;
+DROP TABLE IF EXISTS rooms;
+
+CREATE TABLE data (
+  sample_time   datetime NOT NULL PRIMARY KEY,
+  sample_epoch  integer,
+  room_id       integer,
+  temperature   real,
+  humidity      real,
+  voltage       real
+  );
+
+CREATE TABLE rooms (
+    room_id     text NOT NULL PRIMARY KEY,
+    name        text
+  );
+
+# Groundfloor
+INSERT INTO rooms VALUES('0.0', 'hal')
+INSERT INTO rooms VALUES('0.1.0', 'woonkamer voor')
+INSERT INTO rooms VALUES('0.1.1', 'woonkamer achter')
+INSERT INTO rooms VALUES('0.2', 'keuken')
+INSERT INTO rooms VALUES('0.5', 'schuur')
+INSERT INTO rooms VALUES('0.9.0', 'buiten voor')
+INSERT INTO rooms VALUES('0.9.1', 'buiten achter')
+# 1st floor
+INSERT INTO rooms VALUES('1.0', 'slaapkamer 1')
+INSERT INTO rooms VALUES('1.1', 'badkamer')
+INSERT INTO rooms VALUES('1.2', 'slaapkamer 2')
+INSERT INTO rooms VALUES('1.3', 'slaapkamer 3')
+INSERT INTO rooms VALUES('1.9', 'overloop')
+# 2nd floor
+INSERT INTO rooms VALUES('2.0', 'zolder')
+INSERT INTO rooms VALUES('2.1', 'slaapkamer 4')
+
+# SQLite3 automatically creates a UNIQUE INDEX on the PRIMARY KEY in the background.
+# So, no index needed.
