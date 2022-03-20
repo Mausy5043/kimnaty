bd9c1f8bbbad191c52750a0798c326ed62b8edc0
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-03-20 12:11:30 +0100

    update

--- bin/kimnaty.py
+++ bin/kimnaty.py
@@ -83,28 +83,11 @@ def main():
     while not killer.kill_now:
         if time.time() > pause_time:
             start_time = time.time()
-            result = do_work()
-            result = result.split(",")
-            mf.syslog_trace(f"Result   : {result}", False, DEBUG)
-            # data.append(list(map(int, result)))
-            data.append([int(d) for d in result])
-            if len(data) > samples_averaged:
-                data.pop(0)
-            # mf.syslog_trace(f"Data     : {data}", False, DEBUG)
+            results = do_work(list_of_macs)
+            mf.syslog_trace(f"Result   : {results}", False, DEBUG)
 
-            # report sample average
-            if start_time % report_time < sample_time:
-                # # somma       = list(map(sum, zip(*data)))
-                # somma = [sum(d) for d in zip(*data)]
-                # # not all entries should be float
-                # # ['3088596', '3030401', '270', '0', '0', '0', '1', '1']
-                # # averages    = [format(sm / len(data), '.2f') for sm in somma]
-                # averages = data[-1]
-                # averages[2] = int(somma[2] / len(data))  # avg powerin
-                # averages[5] = int(somma[5] / len(data))  # avg powerout
-                # mf.syslog_trace(f"Averages : {averages}", False, DEBUG)
-                # if averages[0] > 0:
-                do_add_to_database(data, fdatabase, sqlcmd)
+            # report samples
+            do_add_to_database(results, fdatabase, sqlcmd)
 
             pause_time = (sample_time
                           - (time.time() - start_time)
@@ -128,13 +111,16 @@ def do_work(mac_list):
     """Scan the devices to get current readings."""
     block = list()
     retry_list = list()
+    # mac_list = [[mac, room_id], [..., ...], ...]
     for mac in mac_list:
-        block.append = get_data(mac)
+        succes, data = get_data(mac[0])
+        data[0] = mac[1]  # replace mac-address by room-id
+        if succes:
+            block.append = data
 
     return f"{block}"
 
 
-# noinspection PyUnresolvedReferences
 def get_data(mac):
     """Fetch a data from a device."""
     temperature = 0.0
@@ -159,23 +145,6 @@ def get_data(mac):
         mf.syslog_trace("*** This error occured:", syslog.LOG_DEBUG, DEBUG)
         mf.syslog_trace(f"    {e}", syslog.LOG_DEBUG, DEBUG)
 
-        loops2go = loops2go - 1
-        if loops2go < 0:
-            abort = 3
-
-    # test for correct start of telegram
-    if telegram[0][0] != "/":
-        abort = 2
-
-    if abort == 1:
-        with open("/tmp/kimnaty.raw", "w") as output_file:
-            for line in telegram:
-                output_file.write(f"{line}\n")
-
-    # Return codes:
-    # abort == 1 indicates a successful read
-    # abort == 2 means that a serial port read/write error occurred
-    # abort == 3 no valid data after several attempts
     return success, [mac, temperature, humidity, voltage]
 
 
@@ -267,21 +236,6 @@ def test_db_connection(fdatabase):
 if __name__ == "__main__":
     # initialise logging
     syslog.openlog(ident=f'{MYAPP}.{MYID.split(".")[0]}', facility=syslog.LOG_LOCAL0)
-
-    # noinspection PyUnresolvedReferences
-    PORT = serial.Serial()  # pylint: disable=C0103
-    PORT.baudrate = 9600
-    # noinspection PyUnresolvedReferences
-    PORT.bytesize = serial.SEVENBITS
-    # noinspection PyUnresolvedReferences
-    PORT.parity = serial.PARITY_EVEN
-    # noinspection PyUnresolvedReferences
-    PORT.stopbits = serial.STOPBITS_ONE
-    PORT.xonxoff = 1
-    PORT.rtscts = 0
-    PORT.dsrdtr = 0
-    PORT.timeout = 15
-    PORT.port = "/dev/ttyUSB0"
 
     if OPTION.debug:
         DEBUG = True
