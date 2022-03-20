773aef373c831ab00b5640bc277f965fcd5788ac
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-03-20 14:18:38 +0100

    for testing

--- bin/constants.py
+++ bin/constants.py
@@ -35,7 +35,7 @@ KIMNATY = {'database': _DATABASE,
                           ") "
                           "VALUES (?, ?, ?, ?, ?, ?)",
            'sql_table': "data",
-           'report_time': 300,
+           'report_time': 60,
            'cycles': 1,
            'samplespercycle': 1
            }
