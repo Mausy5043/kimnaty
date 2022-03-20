5f5bf6d4cea5f8e1a0746e7cb29095fe504d99d7
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-03-20 09:15:42 +0100

    clean up

--- bin/lywsd03mmc/lywsd03mmc.py
+++ bin/lywsd03mmc/lywsd03mmc.py
@@ -15,8 +15,9 @@ class Sensor3Data(collections.namedtuple('Sensor3DataBase', ['temperature', 'hum
 
 class Lywsd03mmcClient(Lywsd02Client):
     """
-    Temperature units specific to LYWSD03MMC devices
+    Class to connect to sensor and read its data.
     """
+    # Temperature units specific to LYWSD03MMC devices
     UNITS = {
         b'\x01': 'F',
         b'\x00': 'C'
@@ -25,6 +26,13 @@ class Lywsd03mmcClient(Lywsd02Client):
         'F': b'\x01',
         'C': b'\x00'
     }
+
+    # Locally cache the start time of the device.
+    # This value won't change, and caching improves the performance getting the history data
+    _start_time = False
+
+    # Getting history data is very slow, so output progress updates
+    enable_history_progress = True
 
     def __init__(self, mac, notification_timeout=15.0):
         """
@@ -47,20 +55,19 @@ class Lywsd03mmcClient(Lywsd02Client):
         # Estimate the battery percentage remaining
         # CR2024 maximum theoretical voltage = 3.400V
         battery = round(((voltage - 2.1) / (3.4 - 2.1) * 100), 1)
-        # battery = min(int(round((voltage - 2.1), 2) * 100), 100)  # 3.1 or above --> 100% 2.1 --> 0 %
         self._data = Sensor3Data(temperature=temperature, humidity=humidity, battery=battery, voltage=voltage)
 
     @property
     def battery(self):
         """
         Battery data comes along with the temperature and humidity data, so just get it from there
-        :return: guestimate of  battery percentage
+        :return: guestimate of battery percentage
         """
         return self.data.battery
 
     def _get_history_data(self):
         # Get the time the device was first run
-        self.start_time
+        # self.start_time
 
         # Work out the expected last record we'll be sent from the device.
         # The current hour doesn't appear until the end of the hour, and the time is recorded as
@@ -91,31 +98,30 @@ class Lywsd03mmcClient(Lywsd02Client):
         self._history_data[idx] = [ts, min_temp, min_hum, max_temp, max_hum]
         self.output_history_progress(ts, min_temp, max_temp)
 
-    # Getting history data is very slow, so output progress updates
-    enable_history_progress = False
-
     def output_history_progress(self, ts, min_temp, max_temp):
         if not self.enable_history_progress:
             return
         print("{}: {} to {}".format(ts, min_temp, max_temp))
 
-    # Locally cache the start time of the device.
-    # This value won't change, and caching improves the performance getting the history data
-    _start_time = False
-
-    # Work out the start time of the device by taking the current time, subtracting the time
-    # taken from the device (the run time), and adding the timezone offset.
     @property
     def start_time(self):
+        """
+        Work out the start time of the device by taking the current time, subtracting the time
+        taken from the device (the run time), and adding the timezone offset.
+        :return: the start time of the device
+        """
         if not self._start_time:
             start_time_delta = self.time[0] - datetime(1970, 1, 1) - timedelta(hours=self.tz_offset)
             self._start_time = datetime.now() - start_time_delta
         return self._start_time
 
-    # Disable setting the time and timezone.
-    # LYWSD03MMCs don't have visible clocks
     @property
     def time(self):
+        """
+        Disable setting the time and timezone.
+        LYWSD03MMCs don't have visible clocks
+        :return:
+        """
         return super().time
 
     @time.setter
