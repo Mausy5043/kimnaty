0f4f22cde69b48014121157aef0641c3a701155c
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-03-20 09:31:09 +0100

    new files

---
+++ bin/kimnaty.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import argparse
+import lywsd03mmc
+#
+# parser = argparse.ArgumentParser()
+# parser.add_argument('mac', help='MAC address of LYWSD03 device', nargs='+')
+# args = parser.parse_args()
