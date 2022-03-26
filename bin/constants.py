746d274cd7c74318ae96cb4e8579a58a7305cf9e
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-03-26 22:18:45 +0100

    sensor moved

--- bin/constants.py
+++ bin/constants.py
@@ -38,6 +38,5 @@ KIMNATY = {'database': _DATABASE,
            'samplespercycle': 1
            }
 
-DEVICES = [['A4:C1:38:99:AC:4D', '0.1.0'],
-           ['A4:C1:38:99:AC:4D', '0.1.1']
+DEVICES = [['A4:C1:38:99:AC:4D', '2.4', 'badkamer']
            ]
