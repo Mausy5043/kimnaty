b00ad9495639892f59992c0a77bbe40705960495
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-09-22 10:58:59 +0200

    change room IDs

--- bin/kimnaty.sql
+++ bin/kimnaty.sql
@@ -28,13 +28,9 @@ CREATE TABLE rooms (
 -- Groundfloor
 INSERT INTO rooms VALUES('0.0', 'hal');
 INSERT INTO rooms VALUES('0.1', 'woonkamer voor');
-INSERT INTO rooms VALUES('0.1.1', 'woonkamer achter');
 INSERT INTO rooms VALUES('0.2', 'keuken');
 INSERT INTO rooms VALUES('0.5', 'schuur');
 INSERT INTO rooms VALUES('0.6', 'kweektafel');
-INSERT INTO rooms VALUES('0.7.0', 'kas');
-INSERT INTO rooms VALUES('0.9.0', 'buiten voor');
-INSERT INTO rooms VALUES('0.9.1', 'buiten achter');
 -- 1st floor
 INSERT INTO rooms VALUES('1.0', 'overloop');
 INSERT INTO rooms VALUES('1.1', 'slaapkamer 1');
@@ -42,8 +38,8 @@ INSERT INTO rooms VALUES('1.2', 'slaapkamer 2');
 INSERT INTO rooms VALUES('1.3', 'slaapkamer 3');
 INSERT INTO rooms VALUES('1.4', 'badkamer');
 -- 2nd floor
-INSERT INTO rooms VALUES('2.0', 'zolder');
-INSERT INTO rooms VALUES('2.1', 'slaapkamer 4');
+INSERT INTO rooms VALUES('2.1', 'zolder');
+INSERT INTO rooms VALUES('2.2', 'slaapkamer 4');
 
 
 -- TABLE aircon is used to store data from the DAIKIN airconditioners
