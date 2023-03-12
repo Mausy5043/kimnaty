c1fae8bdda3af829c29534e8f9df58430cb3fe42
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2023-03-12 16:19:30 +0100

    renamed

--- bin/test-lywsd03.py
+++ bin/test-lywsd03.py
@@ -10,7 +10,7 @@ args = parser.parse_args()
 
 for mac in args.mac:
     try:
-        client = pyly.Lywsd03mmcClient(mac)
+        client = pyly.Lywsd03client(mac)
     except Exception as e:  # pylint: disable=W0703
         print(e)
 
