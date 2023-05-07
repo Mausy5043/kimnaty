bcd03e5af2859dccb225c57fce5a130fb7df2f72
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-05-07 08:39:31 +0200

    fix path

--- bin/pastmonth.sh
+++ bin/pastmonth.sh
@@ -7,7 +7,7 @@ HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 
 pushd "${HERE}" >/dev/null || exit 1
 # shellcheck disable=SC1091
-source include.sh
+source ./include.sh
 
 if [ "${MAINTENANCE}" == "-" ]; then
     # do some maintenance
