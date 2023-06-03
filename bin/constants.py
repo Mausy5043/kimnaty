8cb6bfa077878fc9551f812571f5798f7045845f
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-06-03 10:44:21 +0200

    fix: W0612 Unused variable 'dirs' [pylint]

--- bin/constants.py
+++ bin/constants.py
@@ -208,7 +208,7 @@ def get_helper_version():
 
 def find_all(name, path):
     result = []
-    for root, dirs, files in os.walk(path):
+    for root, _, files in os.walk(path):
         if name in files:
             result.append(os.path.join(root, name))
     return result
