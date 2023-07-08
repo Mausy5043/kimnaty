77795a11234b3f0a632c3474495a89dc513abc3f
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-07-08 13:15:46 +0200

    boot_kimnaty must now be called when needed

--- bin/pastyear.sh
+++ bin/pastyear.sh
@@ -5,5 +5,10 @@
 HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 
 pushd "${HERE}" >/dev/null || exit 1
+    # shellcheck disable=SC1091
+    source ./include.sh
+    if [ ! -d "${website_image_dir}" ]; then
+        boot_kimnaty
+    fi
     ./trend.py --months 0
 popd >/dev/null || exit
