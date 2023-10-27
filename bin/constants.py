a3bea27356e3ed7bb61f6da2cd73b612408f3f30
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-10-27 21:34:34 +0200

    fix bandit B108

--- bin/constants.py
+++ bin/constants.py
@@ -39,7 +39,7 @@ if not os.path.isfile(_DATABASE):
 
 if not os.path.isdir(_WEBSITE):
     print("Graphics will be diverted to /tmp")
-    _WEBSITE = "/tmp"
+    _WEBSITE = "/tmp"   # nosec B108
 
 DT_FORMAT = "%Y-%m-%d %H:%M:%S"
 
