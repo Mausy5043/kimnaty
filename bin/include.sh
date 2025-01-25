e9ae123c22495ac68c2d27de0d06d8e4b4051331
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-01-25 11:10:13 +0100

    use virtual environment

--- bin/include.sh
+++ bin/include.sh
@@ -190,7 +190,7 @@ install_kimnaty() {
 
     # install Python3 stuff
     pyenv virtualenv 3.13 kimnaty
-    # pyenv local kimnaty
+    pyenv local kimnaty
     python3 -m pip install --upgrade pip setuptools wheel
     python3 -m pip install --upgrade -r requirements.txt
     echo
